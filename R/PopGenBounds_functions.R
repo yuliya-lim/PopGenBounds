635498a9126685f11af4064f8acf6ccf4c60bccf
nalcala <alcalan@iarc.fr>
2025-01-15 17:58:30 +0100

    add plotting functions

--- R/PopGenBounds_functions.R
+++ R/PopGenBounds_functions.R
@@ -9,6 +9,10 @@
 #' freqs <- matrix(c(1,0.5,0,0.5),nrow=2)
 #' Diff(freqs)
 Diff = function(freqs){
+  if( !all(rowSums(freqs)==1) ){
+    stop("Rows of freqs matrix do not sum to 1. Do rows correspond to populations and columns to alleles?")
+  }
+  
   K=nrow(freqs)
   M = max(colMeans(freqs))
   HS = mean(1-rowSums(freqs**2))
@@ -16,7 +20,15 @@ Diff = function(freqs){
   FST = 1-HS/HT
   GpST= FST*(K-1+HS)/(K-1)/(1-HS)
   D = K/(K-1)*(HT-HS)/(1-HS)
-  return(c(M=M,FST = FST,Gp=GpST,D=D))
+  
+  stats = tibble(statistic=c("FST","G'ST","D"), value = c(FST,GpST,D) )
+  
+  # compute bounds
+  bounds = tibble(statistic=c("FST","G'ST","D") , bound=c(Fup(K=K,M=M), Gpup(K=K,M=M), Dup(K=K,M=M) ) )
+  # compute closeness to upper bound
+  closeness = tibble( statistic=c("FST","G'ST","D"), bound_closeness = stats$value/bounds$bound)
+  
+  return(bind_rows(tibble(statistic="M",value=M),left_join(left_join(stats,bounds),closeness)))
 }
 
 
@@ -52,6 +64,7 @@ Fup = function(K,M){
 Dup = function(K,M){
   s1 = K*M
   res = 1*(s1<=1) + (1-(s1**2-(s1%%1)**2-floor(s1))/(K-1)/(K-1+(s1%%1)**2+(1-s1%%1)**2) )*(s1>1)
+  return(res)
 }
 
 #' Computes the maximal G'ST in terms of the most frequent allele
@@ -92,6 +105,10 @@ ggbounds = function(M,FST,K=2){
   nudge = (mean(M)<0.5)*0.16-(mean(M)>=0.5)*0.25
   MFtmp = dplyr::tibble(M= seq(0.00001,1-0.00001,0.00001),
                         FST= Fup(K,seq(0.00001,1-0.00001,0.00001)) )
+  MGptmp = dplyr::tibble(M= seq(0.00001,1-0.00001,0.00001),
+                        GpST= Gpup(K,seq(0.00001,1-0.00001,0.00001)) )
+  MDtmp = dplyr::tibble(M= seq(0.00001,1-0.00001,0.00001),
+                        D= Dup(K,seq(0.00001,1-0.00001,0.00001)) )
   plot <- ggplot2::ggplot(dplyr::tibble(M=M,FST=FST),ggplot2::aes(x=M,y=FST)) + ggpointdensity::geom_pointdensity() +
     ggplot2::geom_point(data=dplyr::tibble(M=mean(M),FST=mean(FST)),col="red",
                pch=16,size=3,stroke=2) +
@@ -106,6 +123,49 @@ ggbounds = function(M,FST,K=2){
     ggplot2::ylab(expression(italic(F[ST]))) +
     ggplot2::coord_cartesian(xlim=c(0,1),ylim=c(0,1),expand = F) +
     ggplot2::theme_bw()
+  
   if(length(M)>2) plot <- plot + ggplot2::scale_color_viridis_b()
   return(plot)
+}
+
+#' Plots differentiation statistics along with the maximal and minimal values of the statistic
+#'
+#' @param freqs a matrix of allele frequency
+#'
+#' @return A ggplot object
+#' @export
+#'
+#' @examples
+#' popa = matrix(c(0.3,0.3,0.3,0.1,rep(0,4*5),
+#'   rep(0,4),0.3,0.3,0.3,0.1,rep(0,4*4),
+#'   rep(0,4*2),0.3,0.3,0.3,0.1,rep(0,4*3),
+#'   rep(0,4*3),0.3,0.3,0.3,0.1,rep(0,4*2),
+#'   rep(0,4*4),0.3,0.3,0.3,0.1,rep(0,4),
+#'   rep(0,4*5),0.3,0.3,0.3,0.1),ncol=24,byrow = T)
+#' ggfreqtable(popa)
+ggfreqtable = function(freqs){
+  freqs.tmp = freqs
+  colnames(freqs.tmp) = paste0(1:ncol(freqs.tmp))
+  freqs.long = bind_cols(Subpopulation=paste0(1:6),as_tibble(freqs.tmp)) %>% pivot_longer(-Subpopulation,names_to = "Allele",values_to = "Frequency")
+
+  freqs.long$Allele = factor(freqs.long$Allele,levels=unique(freqs.long$Allele))
+  freqs.long$Subpopulation = factor(freqs.long$Subpopulation,levels=rev(unique(freqs.long$Subpopulation)) )
+
+ggres = ggplot(freqs.long,aes(x=Allele,y=Subpopulation,fill=Frequency)) + geom_tile(color="black",size=0.25) + 
+  geom_text(aes(label=Frequency))+
+  theme_bw() + 
+  scale_fill_gradientn(limits=c(0,1),colors=c("white","darkorange","darkmagenta"))+
+  theme(
+    #bold font for legend text
+    legend.text=element_text(face="bold"),
+    #set thickness of axis ticks
+    axis.ticks=element_line(size=0.4),
+    #remove plot background
+    plot.background=element_blank(),
+    #remove plot border
+    panel.border=element_blank(),
+    axis.text.x = element_text(angle = 90,vjust = 0.5,hjust=1)
+  )
+
+ return(ggres)
 }
