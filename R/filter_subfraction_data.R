151e8bc562a8fc626991f95c76caaa5a4fb56da0
Yuliya Lim <limy@iarc.who.int>
2025-06-03 14:50:06 +0200

    Add function to filter dataset of allele subfractions

---
+++ R/filter_subfraction_data.R
@@ -0,0 +1,65 @@
+filter_data <- function(df, K=2, filter_fixed=T) {
+  # filter out loci form other samples
+  if (K == 2){
+    subpop_names <- sub(".*\\.", "", colnames(df)[3:4])
+  }
+
+  else if (K == 3) {
+    subpop_names <- sub(".*\\.", "", colnames(df)[3:5])
+  }
+  cat("Subpopulation: ", subpop_names, "\n")
+  cat("Initial data dimension: ", dim(df), "\n")
+
+  data_filtered <- df[df$Sample %in% subpop_names,]
+  cat("After sample name filtering: ", dim(data_filtered), "\n")
+
+  # Clean duplicated rows
+  # number of rows for each chr position (locus)
+  #plot(as.vector(table(data_filtered$pos)))
+  pos_before <- names(table(data_filtered$pos))
+
+  # keep only one of rows in identical locus
+  data_deduped <- data_filtered %>%
+    distinct(pos, .keep_all = TRUE)
+
+  # check number of rows for each chr position (locus) after the deduplication
+  #plot(as.vector(table(data_deduped$pos)))
+  pos_after <- names(table(data_deduped$pos))
+  cat("After de-doubling: ", dim(data_deduped), "\n")
+
+  # check whether after de-duplication we didn't lose any loci
+  are_equal <- setequal(pos_after, pos_before)
+  cat("No locus is lost: ", are_equal, "\n")
+
+  ## Filter out Clonal loci
+  data_filtered <- subset(data_deduped, Clonal == "FALSE")
+  cat("After filtering clonal loci: ", dim(data_filtered), "\n")
+
+  if (filter_fixed == T) {
+    ## Filter loci with 0 frequency in both subclones
+    if (K == 2) {
+      data_filtered <- data_filtered[!(data_filtered[[3]] == 0 & data_filtered[[4]] == 0),]
+    }
+    else if (K == 3) {
+      data_filtered <- data_filtered[
+        !(data_filtered[[3]] == 0 & data_filtered[[4]] == 0 & data_filtered[[5]] == 0),]
+    }
+
+    cat("After filtering loci with no mutations", dim(data_filtered), "\n")
+
+    ## Count the number of loci with fixed alleles
+    if (K == 2) {
+      cat("Loci having at least one fixed subpop", sum(data_filtered[[3]] == 0 |
+                                                         data_filtered[[4]] == 0),
+          "\n\n")
+    }
+
+    else if (K == 3) {
+      cat("Loci having at least one fixed subpop", sum(data_filtered[[3]] == 0 |
+                                                         data_filtered[[4]] == 0 |
+                                                         data_filtered[[5]] == 0), "\n\n")
+    }
+  }
+
+  return(data_filtered)
+}
